853b2deae22756d019f4a933da2b899091e71eb1
Starlight13 <pavlo.p@shopcommerce.nl>
2022-05-09 20:08:51 +0200

    Initial Commit

---
+++ Logotime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logotime
+//
+//  Created by dsadas asdasd on 09.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
